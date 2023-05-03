47c7c27fb4774852ac368502b6d48ffae6b0177e
nav-aiditalhub <nav@aidigitalhub.com>
2023-05-03 17:27:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,28 +2,18 @@ import os
 import http.server
 import socketserver
 
-# from http import HTTPStatus
+from http import HTTPStatus
 
 
-# class Handler(http.server.SimpleHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(HTTPStatus.OK)
-#         self.end_headers()
-#         msg = 'Hello! Everyone, you requested %s' % (self.path)
-#         self.wfile.write(msg.encode())
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        msg = 'Hello! Everyone, you requested %s' % (self.path)
+        self.wfile.write(msg.encode())
 
 
-# port = int(os.getenv('PORT', 80))
-# print('Listening on port Nav %s' % (port))
-# httpd = socketserver.TCPServer(('', port), Handler)
-# httpd.serve_forever()
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-   return 'Hello Tutorialspoint'
-
-if __name__ == '__main__':
-   app.run()
+port = int(os.getenv('PORT', 80))
+print('Listening on port Nav %s' % (port))
+httpd = socketserver.TCPServer(('', port), Handler)
+httpd.serve_forever()
